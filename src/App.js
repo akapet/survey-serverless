32a53e96d27c5a51a9004739b16937a56eb12ea9
Peter Akarakiri <p.akarakiri@gmail.com>
2018-10-29 15:09:57 -0700

    refactored Question into own class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button, Grid, Header, Form, Divider, Checkbox, Icon, Progress } from 'semantic-ui-react'
+import Question from './Question'
 
 class App extends Component {
-
   constructor(props) {
    super(props);
    this.state = {
@@ -17,86 +17,17 @@ class App extends Component {
 
   render() {
     return (
-      <div className="login-form">
+      <div className="question-form">
         <style>{`
           body > div,
           body > div > div,
-          body > div > div > div.login-form {
+          body > div > div > div.question-form {
             height: 100%;
           }
         `}</style>
 
         <Grid relaxed columns={2}  textAlign='center' style={{ height: '100%' }} verticalAlign='middle' padded="horizontally">
-          <Grid.Row style={{ maxWidth: 600 }} >
-            <Grid.Column verticalAlign="top" width={2} stretched={true}>
-              <Grid.Row color="green" style={{height: "15em"}}>
-                <span style={{fontSize:"5em", color: "#3E3E3E"}}>1.</span>
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={14} textAlign="left">
-              <Grid.Row color="blue" >
-                <span style={{fontSize:"1em", color: "grey"}}>Question 1 of 2</span>
-              </Grid.Row>
-
-              <Grid.Row>
-                <Header as="h1">What phone do you use?</Header>
-              </Grid.Row>
-              <Divider hidden/>
-              <Grid.Row>
-                <Form>
-                  <Form.Field>
-                    <Checkbox
-                      label='iPhone'
-                      name='checkboxRadioGroup'
-                      value='iPhone'
-                      checked={this.state.value === 'iPhone'}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <Checkbox
-                      label='Android'
-                      name='checkboxRadioGroup'
-                      value='Android'
-                      checked={this.state.value === 'Android'}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <Checkbox
-                      label='Windows Phone'
-                      name='checkboxRadioGroup'
-                      value='WindowsPhone'
-                      checked={this.state.value === 'WindowsPhone'}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <Checkbox
-                      label='Other'
-                      name='checkboxRadioGroup'
-                      value='Other'
-                      checked={this.state.value === 'Other'}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Field>
-                </Form>
-              </Grid.Row>
-              <Divider hidden/>
-              <Grid.Row>
-                <Button icon labelPosition='right' color='grey'>
-                  NEXT
-                  <Icon name='right arrow' />
-                </Button>
-              </Grid.Row>
-              <Divider hidden/>
-              <Grid.Row>
-                <Progress percent={50} size='tiny' indicating color='orange'>
-                  <span style={{color:"grey", fontSize: "0.7em"}}>50%</span>
-                </Progress>
-              </Grid.Row>
-            </Grid.Column>
-          </Grid.Row>
+          <Question questionNumber={1} totalQuestions={2} title="What phone do you use?" />
         </Grid>
       </div>
     );
